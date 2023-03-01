645cd9103fdea6c1d519d3b3382533d9ff026847
Obaayaa-1988 <aquaicoe31@gmail.com>
2023-03-01 23:09:36 +0000

    added arithmetics and parameters for lasagna functions

--- lasagna/lasagna.js
+++ lasagna/lasagna.js
@@ -33,8 +33,8 @@
 /**
  * The number of minutes it takes to prepare a single layer.
  */
-const PREPARATION_MINUTES_PER_LAYER = 2;
-const EXPECTED_MINUTES_IN_OVEN = 40;
+export const PREPARATION_MINUTES_PER_LAYER = 2;
+export const EXPECTED_MINUTES_IN_OVEN = 40;
 
 /**
  * Determines the number of minutes the lasagna still needs to remain in the
@@ -44,7 +44,7 @@ const EXPECTED_MINUTES_IN_OVEN = 40;
  * @returns {number} the number of minutes remaining
  */
 export function remainingMinutesInOven(actualMinutesInOven) {
-  throw new Error('Remove this line and implement the function');
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
 }
 
 /**
@@ -54,7 +54,9 @@ export function remainingMinutesInOven(actualMinutesInOven) {
  * @returns {number} the total preparation time
  */
 export function preparationTimeInMinutes(numberOfLayers) {
-  return
+  return PREPARATION_MINUTES_PER_LAYER * numberOfLayers;
+  
+  
 }
 
 /**
@@ -66,5 +68,5 @@ export function preparationTimeInMinutes(numberOfLayers) {
  * @returns {number} the total working time
  */
 export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
-  throw new Error('Remove this line and implement the function');
+  return numberOfLayers * PREPARATION_MINUTES_PER_LAYER + actualMinutesInOven
 }
